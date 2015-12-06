79bf4745074c34470e7bb1545817e06220e6e1ce
Kevin Simon <kevin@lawn-128-61-75-150.lawn.gatech.edu>
2015-12-06 16:02:59 -0500

    Added basic symbol detection.

--- meta.scm
+++ meta.scm
@@ -1,7 +1,25 @@
 ;;; Checks if the given expression begins with specified quoted symbol,
-;;; i.e. begins with ', (is-quoted? '(+ 1 2) '+) == #t
-(define (is-symbol? expr symbol)
-  (if (pair? expr) (eq? (car expr) symbol) #f))
+;;; i.e. begins with ', (starts-with-symbol? '(+ 1 2) '+) == #t
+(define (starts-with-symbol? expr symbol)
+  (if (pair? expr)
+    (eq? (car expr) symbol)
+    #f))
+
+;;; Helper methods to check if expressions start with specific symbols
+(define (quote? expr)
+  (starts-with-symbol? expr 'quote))
+
+(define (lambda? expr)
+  (starts-with-symbol? expr 'lambda))
+
+(define (if? expr)
+  (starts-with-symbol? expr 'if))
+
+(define (define? expr)
+  (starts-with-symbol? expr 'define))
+
+(define (cond? expr)
+  (starts-with-symbol? expr 'cond))
 
 ;;; Determines if the expression is already in a form that can be evaluated.
 ;;; i.e. single numerics, strings, etc.
@@ -14,6 +32,15 @@
 
 ;;; One of two main entry points to metacircular evaluator. Evaluates
 ;;; expressions. todo: learn more about this
-(define (eval expr env)
+(define (meval expr env)
   (cond ((self-evaluating? expr) expr)
         (else "Eval failed/Not implemented")))
+
+;;; Driver for REPL, just calls recursive driver with initial environment.
+(define (driver)
+  (recDriver user-initial-environment))
+
+(define (recDriver env)
+  (let ((expr (read))
+        (newEnv (meval expr env)))
+    (recDriver newEnv)))
