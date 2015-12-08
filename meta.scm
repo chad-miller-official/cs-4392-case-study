af11ed2216c1e9efb6fb5a22c976abfcfb490a31
Alexander Langford <nirvashtzero@gmail.com>
2015-12-08 18:56:51 -0500

    Working on lambda and making procedures

--- meta.scm
+++ meta.scm
@@ -1,34 +1,41 @@
-;;; Environment stuff
+;;; Environment definition
 ;;; Relies on environment functions implemented in mit-scheme
 
 (define global-environment (list user-initial-environment))
+
 ;;; One of two main entry points to metacircular evaluator. Evaluates
-;;; expressions. todo: learn more about this
+;;; expressions. 
 (define (meval expr envs)
   (cond ((self-evaluating? expr) expr)
 	((variable? expr) (lookup-var envs expr))
         ((quote? expr) (cadr expr))
-
-        ((lambda? expr) (display "\n;Value: Is lambda\n"))
-        ((if? expr) (display "\n;Value: Is if\n"))
-        ((define? expr) (meval-define (car (cdr expr)) (car (cdr (cdr expr))) envs))
+        ((lambda? expr) (meval-lambda expr envs))
+        ((if? expr) (meval-if (cdr expr) envs))
+        ((define? expr) (meval-define 
+			  (car (cdr expr)) 
+			  (car (cdr (cdr expr))) 
+			  envs))
         ((cond? expr) (display "\n;Value: Is cond\n"))
-	((apply? expr) (mapply (car expr) (cdr expr)))
+	((apply? expr) (mapply 
+			 (car expr)
+			 (list-of-eval-args (cdr expr) envs)))
         (else (error "eval failed/not implemented\n"))))
+
 
 ;;; Other main entry point into metacircular evaluator. Applys arguments
 ;;; to a procedures.
 (define (mapply procedure arguments)
   (cond ((primitive-procedure? procedure)
-         (display "primitive")
 	 (apply-primitive-procedure procedure arguments))
 	((compound-procedure? procedure)
-     (display "compound")
-	 (apply-compount-procedure procedure arguments))
+	 (display "compound")
+	 (apply-compound-procedure procedure arguments))
 	(else
 	  (error "wat"))))
 
+;;; //////////////////////////
 ;;; Eval's condition checks
+;;; //////////////////////////
 
 ;;; Determines if the expression is already in a form that can be evaluated.
 ;;; i.e. single numerics, strings, etc.
@@ -38,15 +45,17 @@
         ((boolean? expr) #t)
         (else (char? expr))))
 
-;;; Helper methods to check if expressions start with specific symbols
+;;; Determines if the expression is a variable. A variable is just a symbol.
 (define (variable? expr)
   (symbol? expr))
 
+;;; A quoted expression just starts with the symbol "quote", or "'".
 (define (quote? expr)
   (starts-with-symbol? expr 'quote))
 
 (define (lambda? expr)
   (starts-with-symbol? expr 'lambda))
+
 (define (if? expr)
   (starts-with-symbol? expr 'if))
 
@@ -56,67 +65,114 @@
 (define (cond? expr)
   (starts-with-symbol? expr 'cond))
 
+;;; Checks to see if it is a function application. If it does not match
+;;; the other definitions and is still a pair, it is a function application.
 (define (apply? expr)
   (pair? expr))
 
+;;; Helper methods to check if expressions start with specific symbols
 ;;; Checks if the given expression begins with specified quoted symbol,
-;;; i.e. begins with ', (starts-with-symbol? '(+ 1 2) '+) == #t
+;;; i.e. begins with ', (starts-with-symbol? (+ 1 2) '+) == #t
 (define (starts-with-symbol? expr symbol)
   (if (pair? expr)
     (eq? (car expr) symbol)
     #f))
 
-;; Eval code
+;;; //////////////////////////
+;;; Eval code
+;;; //////////////////////////
 
-;; TODO
+;;; Given an environment, looks up a symbol. If the variable does
+;;; not exist, it throws an error.
 (define (lookup-var envs symbol)
   (define (lookup-recurse envs)
     (if (null? envs)
-      (error "baka")
+      (begin 
+	(display symbol)
+        (display ": ")
+        (error "Symbol does not exist"))
       (let ((result (environment-bound? (car envs) symbol)))
 	(if (eq? result #f)
 	  (lookup-recurse (cdr envs))
 	  (environment-lookup (car envs) symbol)))))
   (lookup-recurse envs))
 
-;;; TODO
+;;; Given an if statement and an environment, evaluates the if statement.
+(define (meval-if expr envs)
+  (let ((condition (car expr))
+	(then-stmt (cadr expr))
+	(else-stmt (caddr expr)))
+    (if (meval condition envs)
+      (meval then-stmt envs)
+      (if (null? else-stmt) (meval else-stmt envs)))))
+
+;;; Given a define statement and an environment,
+;;; inserts variable definition into
 (define (meval-define symbol expr envs)
   (environment-define (car envs) symbol (meval expr envs)))
 
+;;; Given the arguments of a function, evaluates them to their most basic
+;;; level and puts them in a list.
+(define (list-of-eval-args exprs envs)
+  (if (null? exprs)
+    '()
+    (cons (meval (car exprs) envs) (list-of-eval-args (cdr exprs) envs))))
 
-;;; Primitive procedure table, we will look up primitives to get the compiled procedures here
-(define primitive-table
-  (list (list 'car car)
-        (list 'cdr cdr)
-        (list 'cons cons)
-        (list 'null? null?)
-        (list 'list list)
-        ))
-
+;;; //////////////////////////
 ;;; Apply checks
+;;; //////////////////////////
 
 (define (primitive-procedure? proc)
   (cond ((eq? proc 'car) #t)
         ((eq? proc 'cdr) #t)
         ((eq? proc 'cons) #t)
         ((eq? proc 'null?) #t)
-        ((eq? proc 'list) #t)
+	((eq? proc '+) #t)
+	((eq? proc '-) #t)
+	((eq? proc '*) #t)
+	((eq? proc '/) #t)
+	((eq? proc 'list) #t)
+	((eq? proc 'eq?) #t)
+	((eq? proc '=) #t)
 	(else #f)))
 
+;;; //////////////////////////
 ;;; Apply Code
-
-(define (retrieve-procedure proc table)
-  (if (null? (car table))
-    (error "Procedure not found"))
-  (if (eq? proc (car (car table)))
-    (car (cdr (car table)))
-    (retrieve-procedure proc (cdr table))))
+;;; //////////////////////////
 
 (define (apply-primitive-procedure proc args)
-  (display args)
-  (let ((procedure (retrieve-procedure proc primitive-table)))
-        ;;;(evaluated-args (meval (car args) global-environment)))
-    (cons procedure args)))
+  (apply-in-scheme (cons proc args)))
+
+(define (apply-in-scheme expr)
+  (let ((procedure (car expr))
+	(args (cdr expr)))
+    (cond ((eq? procedure 'cons) (cons (car args) (cadr args)))
+	  ((eq? procedure 'car) (car (car args)))
+	  ((eq? procedure 'cdr) (cdr (car args)))
+	  ((eq? procedure 'null?) (null? (car args)))
+	  ((eq? procedure '+) (apply-arithmetic + args 0))
+	  ((eq? procedure '-) (apply-arithmetic - (cdr args) (car args)))
+	  ((eq? procedure '*) (apply-arithmetic * args 1))
+	  ((eq? procedure '/) (apply-arithmetic / (cdr args) (car args)))
+	  ((eq? procedure 'list) (make-list args))
+	  ((eq? procedure 'eq?) (eq? (car args) (cadr args)))
+	  ((eq? procedure '=) (eq? (car args) (cadr args)))
+	  (else
+	    (display "not implemented yet")))))
+
+(define (apply-arithmetic proc args accum)
+  (if (null? args)
+    accum
+    (apply-arithmetic proc (cdr args) (proc accum (car args)))))
+
+(define (make-list args)
+  (if (null? args)
+    '()
+    (cons (car args) (make-list (cdr args)))))
+
+;;; //////////////////////////
+;;; Interpreter Loop
+;;; //////////////////////////
 
 ;;; Driver for REPL, just calls recursive driver with initial environment.
 (define (driver)
